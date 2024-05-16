1319a924e0a57b5c670564e6ed465f3861b07061
emibonezzi <emibonezzi@gmail.com>
2024-05-16 19:01:24 -0400

    Test

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Something went wrong", message: err.message });
 });
 
-// Export the Express app wrapped in serverless-http
+// Export the Express app wrapped in serverless-http ()
 module.exports.handler = serverless(app);
