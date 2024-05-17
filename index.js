8f211043640b885b003df9be6a4c76fadc1a127e
emibonezzi <emibonezzi@gmail.com>
2024-05-16 20:05:06 -0400

    Fixed preflight requests

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const serverless = require("serverless-http");
 const app = express();
 const routes = require("./routes");
 
+// CORS middleware to handle preflight requests
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, Access-Control-Allow-Headers"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use("/", routes);
 
 // Error handling middleware
